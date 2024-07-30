05cfc2dd2cb313957af63ed7f2b324c6e9d87a3a
Rudransh Pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-07-30 18:34:10 +0530

    Create Union of Two Sorted Arrays.java

---
+++ GFG/Union of Two Sorted Arrays.java
@@ -0,0 +1,23 @@
+
+
+//User function Template for Java
+
+//arr1,arr2 : the arrays
+// n, m: size of arrays
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        TreeSet <Integer> s=new TreeSet<>();
+  ArrayList < Integer > Union=new ArrayList<>();
+  for (int i = 0; i < n; i++)
+    s.add(arr1[i]);
+  for (int i = 0; i < m; i++)
+    s.add(arr2[i]);
+  for (int it: s)
+    Union.add(it);
+  return Union;
+    }
+}
+
